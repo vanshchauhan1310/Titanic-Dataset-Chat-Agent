611a6b2bd4e43c0df93cdf02cc69133e2c5cfe2e
Nishchal-Guptaa <153323788+Nishchal-Guptaa@users.noreply.github.com>
2026-02-28 17:06:44 +0530

    .

--- titanic-backend/main.py
+++ titanic-backend/main.py
@@ -70,5 +70,5 @@ def chat_endpoint(request: QueryRequest):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app", host="0.0.0.0", port=port)
